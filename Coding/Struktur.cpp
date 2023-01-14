2f65d4f23bf7e2b0932c7fd6d9ebd5a8ac3fe6d9
Zachrasn <122687206+Zachrasn@users.noreply.github.com>
2023-01-14 23:40:33 +0700

    Add files via upload

---
+++ Coding/Struktur.cpp
@@ -0,0 +1,31 @@
+//Nama  : Zachra Saniyah. N.  
+//Nim   : 2022240039
+//Kelas : SI 1B
+
+#include <iostream>
+#include <string>
+using namespace std;
+struct mahasiswa
+{
+char nim[10];
+char nama[50];
+char alamat[100];
+float ipk;
+};
+int main() {
+mahasiswa mhs;
+cout<<"NIM \t\t: ";
+cin.getline(mhs.nim,15);
+cout<<"Nama \t\t: ";
+cin.getline(mhs.nama,50);
+cout<<"Alamat \t\t: ";
+cin.getline(mhs.alamat,100);
+cout<<"Nilai IPK \t\t: ";cin>>mhs.ipk;
+cout<<endl;
+cout<<"NIM Mhs \t\t: "<<mhs.nim<<endl;
+cout<<"Nama Mhs \t\t: "<<mhs.nama<<endl;
+cout<<"Alamat Mhs \t\t: "<<mhs.alamat<<endl;
+cout<<"Nilai IPK Mhs \t\t: "<<mhs.ipk<<endl;
+cout<<endl;
+}
+
